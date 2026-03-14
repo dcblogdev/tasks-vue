7674e97fc0801010bf6cf1ca95b0e09eafd3cde9
David Carr <dave@dcblog.dev>
2026-03-14 18:26:57 +0000

    added filtering

--- src/components/Tasks/TaskList.vue
+++ src/components/Tasks/TaskList.vue
@@ -15,16 +15,16 @@ const emit = defineEmits<{
 
 <template>
   <div v-if="tasks.length > 0">
-
-    
-    <article v-for="(task) in tasks" :key="task.id">
-        <lable>
-            <input type="checkbox" :checked="task.completed" @change="$emit('toggleTask', task['id'])" />
-            <span :class="{ completed: task.completed }">{{ task['title'] }}</span>
-        </lable>
-    
-        <button class="secondary" type="button" @click="$emit('deleteTask', task['id'])">Delete</button>
-    </article>
+    <TransitionGroup name="list" tag="div">
+        <article v-for="(task) in tasks" :key="task.id">
+            <lable>
+                <input type="checkbox" :checked="task.completed" @change="$emit('toggleTask', task['id'])" />
+                <span :class="{ completed: task.completed }">{{ task['title'] }}</span>
+            </lable>
+        
+            <button class="secondary" type="button" @click="$emit('deleteTask', task['id'])">Delete</button>
+        </article>
+    </TransitionGroup>
   
     <button type="button" @click="$emit('clearTasks')">Clear Tasks</button>
 
@@ -45,5 +45,16 @@ article {
 article button {
     font-size: 0.75rem;
     padding: 2px 6px;
+}
+
+.list-enter-active,
+.list-leave-active {
+    transition: all 0.3s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
 }
 </style>
--- src/components/Tasks/Tasks.vue
+++ src/components/Tasks/Tasks.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import TaskForm from "./TaskForm.vue";
 import TaskList from "./TaskList.vue";
 import type { Task } from "../../types";
+import type { TaskFilter } from "../../types";
 
 const tasks = ref<Task[]>([]);
+const filter = ref<TaskFilter >('all')
+const pendingTasks = computed(() => tasks.value.filter((item) => !item.completed));
+const completedTasks = computed(() => tasks.value.filter((item) => item.completed));
 
 function store(task: string) {
   tasks.value.push({
@@ -28,7 +32,7 @@ function toggleTask(id: string) {
             return { ...item, completed: !item.completed };
         }
         return item;
-    });
+    }); 
 }
 
 </script>
@@ -36,11 +40,26 @@ function toggleTask(id: string) {
 <template>
     <TaskForm @add-task="store" />
     <h3 v-if="!tasks.length">Add a task</h3>
-    <h3 v-else>{{ tasks.filter((item) => item.completed).length }} / ({{ tasks.length }}) tasks completed</h3>
+    <h3 v-else>{{ completedTasks.length }} / ({{ tasks.length }}) tasks completed</h3>
+    <div v-if="tasks.length" class="button-container">
+        <button :class="{ active: filter === 'all' }" class="outline" @click="filter = 'all'">All</button>
+        <button :class="{ active: filter === 'pending' }" class="outline" @click="filter = 'pending'">Pending</button>
+        <button :class="{ active: filter === 'completed' }" class="outline" @click="filter = 'completed'">Completed</button>
+    </div>
     <TaskList
-        :tasks
+        :tasks="filter === 'all' ? tasks : filter === 'pending' ? pendingTasks : completedTasks"
         @clear-tasks="clearTasks"
         @toggle-task="toggleTask"
         @delete-task="deleteTask"
     />
 </template>
+
+<style>
+.button-container {
+    display: flex;
+    gap: 8px;
+    margin-bottom: 16px;
+    justify-content:end;
+    
+}
+</style>
